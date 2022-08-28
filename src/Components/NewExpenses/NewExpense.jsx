5e6d41dad6af7073403ec117e25a5f1352e7f12f
ffrani <fliksauskas22@gmail.com>
2022-08-28 13:17:37 +0200

    adding form button handler

--- src/Components/NewExpenses/NewExpense.jsx
+++ src/Components/NewExpenses/NewExpense.jsx
@@ -1,20 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 
 import ExpenseForm from "./ExpenseForm";
 import "../Styles/NewExpenses.css";
 
 const NewExpense = (props) => {
+  const [addNewExpense, setAddNewExpense] = useState(false);
+
   const saveExpenseDataHandler = (enteredExpenseData) => {
     const expenseData = {
       ...enteredExpenseData,
       id: Math.random().toString(),
     };
     props.onAddExpense(expenseData);
+    setAddNewExpense(false);
+  };
+
+  const startAddingHandler = () => {
+    setAddNewExpense(true);
+  };
+
+  const stopAddingHandler = () => {
+    setAddNewExpense(false);
   };
 
   return (
     <div className="new-expense">
-      <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
+      {!addNewExpense && (
+        <button onClick={startAddingHandler}>Add new expense</button>
+      )}
+      {addNewExpense && (
+        <ExpenseForm
+          onSaveExpenseData={saveExpenseDataHandler}
+          onCancel={stopAddingHandler}
+        />
+      )}
     </div>
   );
 };
