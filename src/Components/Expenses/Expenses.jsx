7ad5afce26afb538fdddd11d43da3bd463d5b1f8
ffrani <fliksauskas22@gmail.com>
2022-08-30 14:41:31 +0200

    adding chart to filter

--- src/Components/Expenses/Expenses.jsx
+++ src/Components/Expenses/Expenses.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import Card from "../UI/Card";
 import ExpensesFilter from "./ExpensesFilter";
 import ExpensesList from "./ExpensesList";
+import ExpensesChart from "./ExpensesChart";
 import "../Styles/Expenses.css";
 
 const Expenses = (props) => {
@@ -22,6 +23,7 @@ const Expenses = (props) => {
           filterYear={filterYear}
           onChangeFilter={filterYearHandler}
         />
+        <ExpensesChart expenses={filteredExpenses} />
         <ExpensesList items={filteredExpenses} />
       </Card>
     </li>
